84b3a19e1c72219a0c84193d398efe080d031a9d
distributivgesetz <distributivgesetz93@gmail.com>
2021-10-28 17:42:09 +0200

    initial commit

---
+++ SeasonalHatsPatch.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace TooSpookyForMe
+{
+	[HarmonyPatch(typeof(SeasonalHats), "Start")]
+	public static class SeasonalHatsPatch
+	{
+		static bool Prefix()
+		{
+			return false;
+		}
+	}
+}
---
+++ TooSpookyForMe.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace TooSpookyForMe
+{
+	[BepInPlugin("net.distrilul.2spooky4me", "2Spooky4Me", "1.0")]
+	public class TooSpookyForMe : BaseUnityPlugin
+	{
+		private void Awake()
+		{
+			var harmony = new Harmony("net.distrilul.2spooky4me");
+			harmony.PatchAll();
+		}
+	}
+}
